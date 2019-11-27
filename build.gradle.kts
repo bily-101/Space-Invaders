e14b440dc192c9929a1952d3e3572228063bb3bb
Dmitrii Kishchukov <d.kischukov@gmail.com>
2019-11-27 21:47:03 +0000

    build.gradle.kts edited online with Bitbucket

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,16 @@
-val kotlinVersion = "1.3.41"
-
 buildscript {
-    val kotlinVersion = "1.3.41"
-
     repositories {
         mavenCentral()
         jcenter()
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.1")
     }
 }
 
 plugins {
-    kotlin("jvm") version "1.3.41"
+    kotlin("jvm") version "1.3.50"
 }
 
 repositories {
@@ -25,8 +20,8 @@ repositories {
 
 dependencies {
     compile(gradleApi())
-    compile(kotlin("stdlib", kotlinVersion))
+    compile(kotlin("stdlib"))
     compile(kotlin("stdlib-jdk8"))
-    compile(kotlin("reflect", kotlinVersion))
-    compile("com.anysolo:toyGraphics:0.8.4")
+    compile(kotlin("reflect"))
+    compile("com.anysolo:toyGraphics:0.8.6.4")
 }
